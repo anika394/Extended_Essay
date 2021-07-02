01c7af31f6911f5a2ce51729970f04edebc4f187
anika394 <63273768+anika394@users.noreply.github.com>
2021-07-02 11:32:41 +0800

    Update

--- Project_Node/app.js
+++ Project_Node/app.js
@@ -1,14 +1,36 @@
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Create a server
+http.createServer( function (request, response) {  
+   // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+   
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      } else {	
+         //Page found	  
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+         // Write the content of the file to response body
+         response.write(data.toString());		
+      }
+      
+      // Send the response body 
+      response.end();
+   });   
+}).listen(8081);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log('Server running at http://127.0.0.1:8081/');
